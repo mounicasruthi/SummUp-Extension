5dd62233d9361a8777c2f31a6b8f8594c3b87901
Mounica <mounicasruthi30@gmail.com>
2023-05-14 11:58:31 +0530

    added question answer functionality

--- scripts/background.js
+++ scripts/background.js
@@ -3,7 +3,7 @@ async function summarize(articleText) {
     // Set up the API request
     const requestHeaders = new Headers({
       "Content-Type": "application/json",
-      "Authorization": "Bearer <your_openai_api_key>"
+      "Authorization": "Bearer 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"
     });
   
     const requestBody = JSON.stringify({
@@ -34,23 +34,85 @@ async function summarize(articleText) {
     console.log("summary: " , summary);
     return summary;
   }
+
+  // Answer a question based on the article using the OpenAI API
+async function answerQuestion(articleText, question) {
+    // Set up the API request
+    console.log("hi")
+    const requestHeaders = new Headers({
+      "Content-Type": "application/json",
+      "Authorization": "Bearer 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"
+    });
+
+    console.log(question)
+    console.log(articleText)
+  
+    const requestBody = JSON.stringify({
+        "model": "text-davinci-003",
+      "prompt": `Here's the content of the webpage: ${articleText}. Based on that, I need you to provide me an answer the following question? ${question}`,
+      "temperature": 0.5,
+      "max_tokens": 256,
+      "top_p": 1,
+      "frequency_penalty": 0,
+      "presence_penalty": 0
+    });
+
+    console.log(requestBody, "umm?")
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: requestHeaders,
+      body: requestBody,
+      redirect: 'follow'
+    };
+  
+    // Send the API request
+    const response = await fetch("https://api.openai.com/v1/completions", requestOptions);
+    const jsonResponse = await response.json();
+
+    console.log(jsonResponse);
+  
+    // Extract the answer from the API response
+    const answer = jsonResponse.choices[0].text.trim();
+    return answer;
+  }
+  
+  
   
   // Listen for messages from the popup
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === "summarize") {
       const articleText = request.articleText;
   
       // Call the summarize function
       summarize(articleText)
-        .then(summary => {
-          sendResponse({ summary });
-          chrome.runtime.sendMessage({ summary });
-        })
-        .catch(error => {
-          sendResponse({ error: error.message });
-        });
+      .then(summary => {
+        sendResponse({ summary });
+        chrome.runtime.sendMessage({ summary });
+      })
+      .catch(error => {
+        sendResponse({ error: error.message });
+      });
+  
+      return true;
+    }
+    else if (request.message === "answer") {
+      const articleText = request.articleText;
+      const question = request.question;
+
+      console.log(question, "outside")
+  
+      // Call the answerQuestion function
+      answerQuestion(articleText, question)
+      .then(answer => {
+        console.log(question, "inside")
+        sendResponse({ answer });
+        chrome.runtime.sendMessage({ answer });
+      })
+      .catch(error => {
+        sendResponse({ error: error.message });
+      });
   
       return true;
     }
   });
-  
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,52 +1,100 @@
 // // Send a message to the content script to get the article text
 // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     chrome.tabs.sendMessage(tabs[0].id, { message: "getArticleText" }, (response) => {
-//         console.log("hello");
-//         //  console.log(response.articleText)
-//         // console.log(message);
-//       const articleText = response.articleText;
+//   chrome.tabs.sendMessage(tabs[0].id, { message: "getArticleText" }, (response) => {
+//     // console.log(response.articleText)
+//     // console.log(message);
+//     const articleText = response.articleText;
+//     document.getElementById("summarize-btn").addEventListener("click", () => {
 //       summarizeArticle(articleText);
 //     });
+
+//     document.getElementById("ask-btn").addEventListener("click", () => {
+//       getAnswer(articleText);
+//     });
+
 //   });
-//   // console.log("peekabooo")
-//   // Send a message to the background script to summarize the article
-//   function summarizeArticle(articleText) {
-//     console.log("peekabooo")
-//     chrome.runtime.sendMessage({ message: "summarize", articleText: articleText });
+// });
+
+// // Send a message to the background script to summarize the article
+// function summarizeArticle(articleText) {
+//   chrome.runtime.sendMessage({ message: "summarize", articleText: articleText });
+// }
+
+// // const questionInput = document.getElementById('question-input');
+// // const question = questionInput.value;
+// // function getAnswer(articleText, question) {
+// //   chrome.runtime.sendMessage({ message: "answer", articleText: articleText, question: question });
+// // }
+
+// // Listen for messages from the background script with the summary
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.summary) {
+//     const summary = request.summary;
+//     displaySummary(summary);
+//   } else if (request.answer) {
+//     const answer = request.answer;
+//     displayAnswer(answer);
 //   }
-  
-//   // Listen for messages from the background script with the summary
-//   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     console.log("do I spot you")
-//     if (request.summary) {
-//       console.log("received summary: ", request.summary);
-//       const summary = request.summary;
-//       displaySummary(summary);
-//     }
-//   });
-  
-//   // // Display the summary in a new tab
-//   // function displaySummary(summary) {
-//   //   const summaryUrl = `data:text/html,<html><body><p>${summary}</p></body></html>`;
-//   //   window.open(summaryUrl, "_blank");
-//   // }
-//   // Define the function that displays the summary
+// });
+
 // const displaySummary = (summary) => {
-//   // Create a new tab with the summary content when the "Summarize" button is clicked
-//   const summaryUrl = `data:text/html,<html><body><p>${summary}</p></body></html>`;
-//   chrome.tabs.create({ url: summaryUrl });
+//   // Load the summary page HTML file
+//   fetch(chrome.runtime.getURL("summary.html"))
+//     .then(response => response.text())
+//     .then(html => {
+//       // Insert the summary content into the page HTML
+//       const summaryHtml = html.replace("{{summary}}", summary);
+      
+//       // Create a new tab with the summary page
+//       chrome.tabs.create({ url: "data:text/html;charset=UTF-8," + encodeURIComponent(summaryHtml) });
+//     })
+//     .catch(error => console.error(error));
 // };
 
+// const displayAnswer = (answer) => {
+//   const answerContainer = document.createElement("div");
+//   answerContainer.setAttribute("id", "answer-container");
+
+//   const answerText = document.createElement("p");
+//   answerText.innerHTML = answer;
+
+//   answerContainer.appendChild(answerText);
+
+//   const questionForm = document.querySelector('form');
+//   questionForm.parentNode.insertBefore(answerContainer, questionForm.nextSibling);
+// };
+
+
+// // Send a message to the background script to get an answer to the question
+// const questionForm = document.querySelector('form');
+// const questionInput = document.getElementById('question-input');
+// const askButton = document.getElementById('ask-button');
+
+// // askButton.removeEventListener('click', handleClick);
+// // askButton.addEventListener('click', handleClick);
+
+// function handleClick(event) {
+//   event.preventDefault();
+//   const question = questionInput.value;
+//   chrome.runtime.sendMessage({ message: "answer", question: question }, (response) => {
+//     const answer = response.answer;
+//     displayAnswer(answer);
+//   });
+//   questionInput.value = '';
+// }
 
 
 // Send a message to the content script to get the article text
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   chrome.tabs.sendMessage(tabs[0].id, { message: "getArticleText" }, (response) => {
-    // console.log(response.articleText)
-    // console.log(message);
     const articleText = response.articleText;
+
     document.getElementById("summarize-btn").addEventListener("click", () => {
       summarizeArticle(articleText);
+    });
+
+    document.getElementById("ask-btn").addEventListener("click", () => {
+      getAnswer(articleText);
     });
   });
 });
@@ -56,20 +104,29 @@ function summarizeArticle(articleText) {
   chrome.runtime.sendMessage({ message: "summarize", articleText: articleText });
 }
 
-// Listen for messages from the background script with the summary
+// Send a message to the background script to get an answer to the question
+function getAnswer(articleText) {
+  const questionInput = document.getElementById('question-input');
+  const question = questionInput.value;
+
+  chrome.runtime.sendMessage({ message: "answer", articleText: articleText, question: question }, (response) => {
+    const answer = response.answer;
+    displayAnswer(answer);
+  });
+
+  // questionInput.value = '';
+}
+
+// Listen for messages from the background script with the summary or answer
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.summary) {
     const summary = request.summary;
     displaySummary(summary);
+  } else if (request.answer) {
+    const answer = request.answer;
+    displayAnswer(answer);
   }
 });
-
-// const displaySummary = (summary) => {
-//   // Create a new tab with the summary content when the "Summarize" button is clicked
-//   const summaryUrl = `data:text/html,<html><head><title>Summary</title></head><body><p>${summary}</p></body></html>`;
-//   chrome.tabs.create({ url: summaryUrl });
-// };
-
 
 const displaySummary = (summary) => {
   // Load the summary page HTML file
@@ -83,4 +140,35 @@ const displaySummary = (summary) => {
       chrome.tabs.create({ url: "data:text/html;charset=UTF-8," + encodeURIComponent(summaryHtml) });
     })
     .catch(error => console.error(error));
+};
+
+// const displayAnswer = (answer) => {
+//   const answerContainer = document.createElement("div");
+//   answerContainer.setAttribute("id", "answer-container");
+
+//   const answerText = document.createElement("p");
+//   answerText.innerHTML = answer;
+
+//   answerContainer.appendChild(answerText);
+
+//   const questionForm = document.querySelector('form');
+//   questionForm.parentNode.insertBefore(answerContainer, questionForm.nextSibling);
+// };
+
+const displayAnswer = (answer) => {
+  const answerContainer = document.createElement("div");
+  answerContainer.setAttribute("id", "answer-container");
+  answerContainer.style.marginTop = "20px";
+  answerContainer.style.padding = "20px";
+  answerContainer.style.border = "1px solid #ddd";
+
+  const answerText = document.createElement("p");
+  answerText.innerHTML = answer;
+  answerText.style.fontSize = "16px";
+  answerText.style.lineHeight = "1.4";
+
+  answerContainer.appendChild(answerText);
+
+  const questionForm = document.querySelector('form');
+  questionForm.parentNode.insertBefore(answerContainer, questionForm.nextSibling);
 };
