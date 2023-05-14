0063a7856e3aa35be09560dc131713a28bf5d9eb
Mounica <mounicasruthi30@gmail.com>
2023-05-14 22:31:36 +0530

    code cleaning

--- scripts/background.js
+++ scripts/background.js
@@ -1,118 +1,121 @@
 // Summarize the article using the OpenAI API
 async function summarize(articleText) {
-    // Set up the API request
-    const requestHeaders = new Headers({
-      "Content-Type": "application/json",
-      "Authorization": "Bearer <your_openai_api_key>"
-    });
-  
-    const requestBody = JSON.stringify({
-        "model": "text-davinci-003",
-      "prompt": `Please summarize the following article:\n\n${articleText}\n\nSummary:`,
-      "temperature": 0.7,
-      "max_tokens": 256,
-      "top_p": 1,
-      "frequency_penalty": 0,
-      "presence_penalty": 0
-    });
-  
-    const requestOptions = {
-      method: 'POST',
-      headers: requestHeaders,
-      body: requestBody,
-      redirect: 'follow'
-    };
-  
-    // Send the API request
-    const response = await fetch("https://api.openai.com/v1/completions", requestOptions);
-    const jsonResponse = await response.json();
-
-    console.log(jsonResponse);
-  
-    // Extract the summary from the API response
-    const summary = jsonResponse.choices[0].text.trim();
-    console.log("summary: " , summary);
-    return summary;
-  }
-
-  // Answer a question based on the article using the OpenAI API
-async function answerQuestion(articleText, question) {
-    // Set up the API request
-    console.log("hi")
-    const requestHeaders = new Headers({
-      "Content-Type": "application/json",
-      "Authorization": "Bearer <your_openai_api_key>"
-    });
-
-    console.log(question)
-    console.log(articleText)
-  
-    const requestBody = JSON.stringify({
-        "model": "text-davinci-003",
-      "prompt": `Here's the content of the webpage: ${articleText}. Based on that, I need you to provide me an answer the following question? ${question}`,
-      "temperature": 0.5,
-      "max_tokens": 256,
-      "top_p": 1,
-      "frequency_penalty": 0,
-      "presence_penalty": 0
-    });
-
-    console.log(requestBody, "umm?")
-  
-    const requestOptions = {
-      method: 'POST',
-      headers: requestHeaders,
-      body: requestBody,
-      redirect: 'follow'
-    };
-  
-    // Send the API request
-    const response = await fetch("https://api.openai.com/v1/completions", requestOptions);
-    const jsonResponse = await response.json();
-
-    console.log(jsonResponse);
-  
-    // Extract the answer from the API response
-    const answer = jsonResponse.choices[0].text.trim();
-    return answer;
-  }
-  
-  
-  
-  // Listen for messages from the popup
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "summarize") {
-      const articleText = request.articleText;
-  
-      // Call the summarize function
-      summarize(articleText)
-      .then(summary => {
-        sendResponse({ summary });
-        chrome.runtime.sendMessage({ summary });
-      })
-      .catch(error => {
-        sendResponse({ error: error.message });
-      });
-  
-      return true;
-    }
-    else if (request.message === "answer") {
-      const articleText = request.articleText;
-      const question = request.question;
-
-      console.log(question, "outside")
-  
-      // Call the answerQuestion function
-      answerQuestion(articleText, question)
-      .then(answer => {
-        console.log(question, "inside")
-        sendResponse({ answer });
-        chrome.runtime.sendMessage({ answer });
-      })
-      .catch(error => {
-        sendResponse({ error: error.message });
-      });
-  
-      return true;
-    }
+  // Set up the API request
+  const requestHeaders = new Headers({
+    "Content-Type": "application/json",
+    "Authorization": "Bearer <your_openai_api_key>"
   });
+
+  const requestBody = JSON.stringify({
+      "model": "text-davinci-003",
+    "prompt": `Please summarize the following article:\n\n${articleText}\n\nSummary:`,
+    "temperature": 0.7,
+    "max_tokens": 256,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: requestHeaders,
+    body: requestBody,
+    redirect: 'follow'
+  };
+
+  // Send the API request
+  const response = await fetch("https://api.openai.com/v1/completions", requestOptions);
+  const jsonResponse = await response.json();
+
+  console.log(jsonResponse);
+
+  // Extract the summary from the API response
+  const summary = jsonResponse.choices[0].text.trim();
+  console.log("summary: " , summary);
+  return summary;
+}
+
+// Answer a question based on the article using the OpenAI API
+async function answerQuestion(articleText, question) {
+  // Set up the API request
+
+  
+  console.log("hi")
+  const requestHeaders = new Headers({
+    "Content-Type": "application/json",
+    "Authorization": "Bearer <your_openai_api_key>"
+  });
+
+  console.log(question)
+  console.log(articleText)
+
+  const requestBody = JSON.stringify({
+      "model": "text-davinci-003",
+    "prompt": `Here's the content of the webpage: ${articleText}. Based on that, I need you to provide me an answer the following question? ${question}`,
+    "temperature": 0.5,
+    "max_tokens": 256,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0
+  });
+
+  console.log(requestBody, "umm?")
+
+  const requestOptions = {
+    method: 'POST',
+    headers: requestHeaders,
+    body: requestBody,
+    redirect: 'follow'
+  };
+
+  // Send the API request
+  const response = await fetch("https://api.openai.com/v1/completions", requestOptions);
+  const jsonResponse = await response.json();
+
+  console.log(jsonResponse);
+
+  // Extract the answer from the API response
+  const answer = jsonResponse.choices[0].text.trim();
+  return answer;
+}
+
+
+
+
+// Listen for messages from the popup
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "summarize") {
+    const articleText = request.articleText;
+
+    // Call the summarize function
+    summarize(articleText)
+    .then(summary => {
+      sendResponse({ summary });
+      chrome.runtime.sendMessage({ summary });
+    })
+    .catch(error => {
+      sendResponse({ error: error.message });
+    });
+
+    return true;
+  }
+  else if (request.message === "answer") {
+    const articleText = request.articleText;
+    const question = request.question;
+
+    console.log(question, "outside")
+
+    // Call the answerQuestion function
+    answerQuestion(articleText, question)
+    .then(answer => {
+      console.log(question, "inside")
+      sendResponse({ answer });
+      chrome.runtime.sendMessage({ answer });
+    })
+    .catch(error => {
+      sendResponse({ error: error.message });
+    });
+
+    return true;
+  }
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,88 +1,3 @@
-// // Send a message to the content script to get the article text
-// chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//   chrome.tabs.sendMessage(tabs[0].id, { message: "getArticleText" }, (response) => {
-//     // console.log(response.articleText)
-//     // console.log(message);
-//     const articleText = response.articleText;
-//     document.getElementById("summarize-btn").addEventListener("click", () => {
-//       summarizeArticle(articleText);
-//     });
-
-//     document.getElementById("ask-btn").addEventListener("click", () => {
-//       getAnswer(articleText);
-//     });
-
-//   });
-// });
-
-// // Send a message to the background script to summarize the article
-// function summarizeArticle(articleText) {
-//   chrome.runtime.sendMessage({ message: "summarize", articleText: articleText });
-// }
-
-// // const questionInput = document.getElementById('question-input');
-// // const question = questionInput.value;
-// // function getAnswer(articleText, question) {
-// //   chrome.runtime.sendMessage({ message: "answer", articleText: articleText, question: question });
-// // }
-
-// // Listen for messages from the background script with the summary
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.summary) {
-//     const summary = request.summary;
-//     displaySummary(summary);
-//   } else if (request.answer) {
-//     const answer = request.answer;
-//     displayAnswer(answer);
-//   }
-// });
-
-// const displaySummary = (summary) => {
-//   // Load the summary page HTML file
-//   fetch(chrome.runtime.getURL("summary.html"))
-//     .then(response => response.text())
-//     .then(html => {
-//       // Insert the summary content into the page HTML
-//       const summaryHtml = html.replace("{{summary}}", summary);
-      
-//       // Create a new tab with the summary page
-//       chrome.tabs.create({ url: "data:text/html;charset=UTF-8," + encodeURIComponent(summaryHtml) });
-//     })
-//     .catch(error => console.error(error));
-// };
-
-// const displayAnswer = (answer) => {
-//   const answerContainer = document.createElement("div");
-//   answerContainer.setAttribute("id", "answer-container");
-
-//   const answerText = document.createElement("p");
-//   answerText.innerHTML = answer;
-
-//   answerContainer.appendChild(answerText);
-
-//   const questionForm = document.querySelector('form');
-//   questionForm.parentNode.insertBefore(answerContainer, questionForm.nextSibling);
-// };
-
-
-// // Send a message to the background script to get an answer to the question
-// const questionForm = document.querySelector('form');
-// const questionInput = document.getElementById('question-input');
-// const askButton = document.getElementById('ask-button');
-
-// // askButton.removeEventListener('click', handleClick);
-// // askButton.addEventListener('click', handleClick);
-
-// function handleClick(event) {
-//   event.preventDefault();
-//   const question = questionInput.value;
-//   chrome.runtime.sendMessage({ message: "answer", question: question }, (response) => {
-//     const answer = response.answer;
-//     displayAnswer(answer);
-//   });
-//   questionInput.value = '';
-// }
-
 
 // Send a message to the content script to get the article text
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -141,19 +56,6 @@ const displaySummary = (summary) => {
     })
     .catch(error => console.error(error));
 };
-
-// const displayAnswer = (answer) => {
-//   const answerContainer = document.createElement("div");
-//   answerContainer.setAttribute("id", "answer-container");
-
-//   const answerText = document.createElement("p");
-//   answerText.innerHTML = answer;
-
-//   answerContainer.appendChild(answerText);
-
-//   const questionForm = document.querySelector('form');
-//   questionForm.parentNode.insertBefore(answerContainer, questionForm.nextSibling);
-// };
 
 const displayAnswer = (answer) => {
   const answerContainer = document.createElement("div");
